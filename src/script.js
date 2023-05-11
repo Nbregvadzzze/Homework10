574466bf3b4c9c83353a3c66bb234a8a9e98836f
Nbregvadzzze <129889614+Nbregvadzzze@users.noreply.github.com>
2023-05-11 22:16:55 +0300

    js

---
+++ src/script.js
@@ -0,0 +1,9 @@
+var salary = 3000;
+
+if (salary < 2000) {
+    alert("gaagde saxlidan");
+}   else if (salary > 2000 && salary < 4000) {
+    alert("sashualo xelfasi");
+}   else {
+    alert("kai xelfasi");
+}
